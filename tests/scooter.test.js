0ca5583b94041f54be2c5433b80519b8faae840e
AGROYGBV <amandagracerenfro@gmail.com>
2021-11-24 13:20:54 -0600

    changed require

--- tests/scooter.test.js
+++ tests/scooter.test.js
@@ -1,14 +1,14 @@
-const Scooter = require("../src/scooter.js/index.js");
+const Scooter = require("../src/scooter");
 
 describe('Scooter class', () => {
     const testScooter = new Scooter("8", "Deep Ellum", "Greenville", "Yes");
 
-    test('user has id, chargeOrigin, chargeDestination, checkedOut', () => {
+    test('user has id, chargeOrigin, chargeDestination, checkedOut, charging stations, and checkedOut method', () => {
         expect(testScooter.id).toBe("8")
         expect(testScooter.chargeOrigin).toBe("Deep Ellum")
         expect(testScooter.chargeDestination).toBe("Greenville")
         expect(testScooter.checkedOut).toBe("Yes")
-        expect(Scooter.chargingStations).toStrictEqual(["Deep Ellum", "Greenville", "Oak Cliff", "Trinity Groves", "Downtown", "Oak Lawn", "Uptown"])
+        expect(Scooter.chargingStations[3]).toStrictEqual("Trinity Groves")
         expect(testScooter.isCheckedOut()).toBe(true)
     });
 });    
